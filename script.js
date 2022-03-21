b61dff8f74a2b007c77496f2801b4c99a8731ffd
Anastacia Muringo <anastasmuringo@gmail.com>
2022-03-21 12:08:56 +0300

    Modified the loan calculator

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let monthlyRepayment = document.querySelector("#total-monthly").innerHTML;
 let numberOfMonths = Number(document.querySelector("#months-number").value);
 //Onclick items
 document.querySelector("#loan-btn").addEventListener("click", displayLoan);
+document.querySelector("#reset-btn").addEventListener("click", loanReset);
+
 // FUNCTIONS
 function displayLoan(){
     if(numberOfMonths > 1){
@@ -15,8 +17,7 @@ function displayLoan(){
         let interestRate = ((newMonths * 0.75) + 1.5);
         let interest = ((loanAmount * interestRate)/100);
         let totalLoanAmount = loanAmount + interest;
-        let loanMonthlyRepayment = Math.round(totalLoanAmount/12);
-        document.querySelector("#total-monthly").innerHTML = loanMonthlyRepayment;
+        let loanMonthlyRepayment = Math.round(totalLoanAmount/numberOfMonths);
+        document.querySelector("#total-monthly").innerHTML =loanMonthlyRepayment;
     }
-    // console.log(Math.round(loanAmount/12))
 }
